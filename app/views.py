85e281902368b7c2e55c978e5e0b8c5f5ef908ed
paulopkhsl <77590894+paulopkhsl@users.noreply.github.com>
2024-04-15 08:12:39 -0300

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -1,3 +1,95 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import *
+from django.views import View
+from django.contrib import messages
 
-# Create your views here.
+
+class TemplateView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'index.html')
+    def post(self, request):
+        pass
+
+
+class OcupacaoView(View):
+    def get(self, request, *args, **kwargs):
+        ocupacao = Ocupacao.objects.all()
+        return render(request, 'ocupacao.html', {'ocupacao':ocupacao})
+
+
+class CidadeView(View):
+    def get(self, request, *args, **kwargs):
+        cidade = Cidade.objects.all()
+        return render(request, 'cidade.html', {'cidade':cidade})
+
+
+class PessoaView(View):
+    def get(self, request, *args, **kwargs):
+        pessoa = Pessoa.objects.all()
+        return render(request, 'pessoa.html', {'pessoa':pessoa})
+
+
+class AreaView(View):
+    def get(self, request, *args, **kwargs):
+        area = Area.objects.all()
+        return render(request, 'area.html', {'area':area})
+
+
+class InstituicaoView(View):
+    def get(self, request, *args, **kwargs):
+        instituicao = Instituicao.objects.all()
+        return render(request, 'instituicao.html', {'instituicao':instituicao})
+
+class CursoView(View):
+    def get(self, request, *args, **kwargs):
+        curso = Curso.objects.all()
+        return render(request, 'curso.html', {'curso':curso})
+
+
+class DisciplinaView(View):
+    def get(self, request, *args, **kwargs):
+        disciplina = Disciplina.objects.all()
+        return render(request, 'disciplina.html', {'disciplina':disciplina})
+
+class PeriodoView(View):
+    def get(self, request, *args, **kwargs):
+        periodo = Periodo.objects.all()
+        return render(request, 'periodo.html', {'periodo':periodo})
+
+class MatriculaView(View):
+    def get(self, request, *args, **kwargs):
+        matricula = Matricula.objects.all()
+        return render(request, 'matricula.html', {'matricula':matricula})
+
+class AvaliacaoView(View):
+    def get(self, request, *args, **kwargs):
+        avaliacao = Avaliacao.objects.all()
+        return render(request, 'avaliacao.html', {'avaliacao':avaliacao})
+
+class FrequenciaView(View):
+    def get(self, request, *args, **kwargs):
+        frequencia = Frequencia.objects.all()
+        return render(request, 'frequencia.html', {'frequencia':frequencia})
+
+
+class TurmaView(View):
+    def get(self, request, *args, **kwargs):
+        turma = Turma.objects.all()
+        return render(request, 'turma.html', {'turma':turma})
+
+class OcorrenciaView(View):
+    def get(self, request, *args, **kwargs):
+        ocorrencia = Ocorrencia.objects.all()
+        return render(request, 'ocorrencia.html', {'ocorrencia':ocorrencia})
+
+
+class Disciplina_cursoView(View):
+    def get(self, request, *args, **kwargs):
+        disciplina_curso = Disciplina_curso.objects.all()
+        return render(request, 'disciplina_curso.html', {'disciplina_cursos':disciplina_curso})
+
+
+class Tipos_avaliacaoView(View):
+    def get(self, request, *args, **kwargs):
+        tipos_avaliacao = Tipos_avaliacao.objects.all()
+        return render(request, 'tipos_avaliacao.html', {'tipos_avaliacao':tipos_avaliacao})
--- config/urls.py
+++ config/urls.py
@@ -1,23 +1,26 @@
-"""
-URL configuration for config project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+ 
 from django.contrib import admin
-from django.urls import include, path
-from app.views import * 
+from django.urls import path
+from django.views.generic import TemplateView
+from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('area/',AreaView.as_view(),name='area'),
+    path('',TemplateView.as_view(),name='index'),
+    path('ocupacao/', OcupacaoView.as_view(), name='ocupacao'),
+    path('cidade/', CidadeView.as_view(),name='cidade'),
+    path('pessoa/', PessoaView.as_view(),name='pessoa'),
+    path('instituicao/', InstituicaoView.as_view(), name='instituicao'),
+    path('curso/', CursoView.as_view(),name='curso'),
+    path('disciplina/', DisciplinaView.as_view(),name='disciplina'),
+    path('periodo/', PeriodoView.as_view(),name='periodo'),
+    path('matricula/', MatriculaView.as_view(),name='matricula'),
+    path('avaliacao/', AvaliacaoView.as_view(),name='avaliacao'),
+    path('frequencia/', FrequenciaView.as_view(),name='frequencia'),
+    path('turma/', TurmaView.as_view(),name='turma'),
+    path('ocorrencia/', OcorrenciaView.as_view(),name='ocorrencia'),
+    path('disciplina_curso/', Disciplina_cursoView.as_view(),name='disciplina_curso'),
+    path('tipos_avaliacao/', Tipos_avaliacaoView.as_view(),name='tipos_avaliacao'),
 ]
